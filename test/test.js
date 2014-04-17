99ba136db55b6de53da79541b08d73b43a7c77bd
logmein3546 <root@katarina.(none)>
2014-04-17 06:09:21 -0400

    implemented, testing to come

---
+++ test/test.js
@@ -0,0 +1,34 @@
+var neverDrop = require('./../neverdrop.js');
+var fs = require('fs');
+
+var clientOptions = {
+	host: 'localhost',
+	port: 3547,
+	rejectUnauthorized: false,
+	ca: [fs.readFileSync('./cert/test.crt')],
+	requestCert: true,
+	agent: false
+}
+
+var serverOptions = {
+	key: fs.readFileSync('./cert/test.key'),
+	cert: fs.readFileSync('./cert/test.crt'),
+	rejectUnauthorized: false,
+	requestCert: true,
+};
+
+var server = neverDrop.createServer(serverOptions, function(socket){
+	for(var i=0; i<100; i++){
+		socket.write(i);
+	}
+});
+server.listen(3547);
+
+var socket = neverDrop.connect(clientOptions);
+socket.on('error', function(e){
+	console.log('error: ', e);
+});
+
+socket.on('message', function(data){
+	console.log('message: ', data);
+});
